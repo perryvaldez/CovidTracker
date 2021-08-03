c49e621efece1c473afb1d57883c16cfe76ef70d
Perry Valdez <perryv@magenic.com>
2021-08-03 23:51:59 +0800

    Revamped notification

--- client/src/components/dashboard/Dashboard.tsx
+++ client/src/components/dashboard/Dashboard.tsx
@@ -4,12 +4,23 @@ import { makeStyles } from '@material-ui/styles';
 import H1 from '../markup/H1';
 import DashboardButtons from './DashboardButtons';
 import styles from './Dashboard.styles';
+import { Box, Grid } from '@material-ui/core';
+import NotificationTray from './NotificationTray';
 
 export const Dashboard: React.FC = () => { 
   const classes = makeStyles(styles)();
   return (
     <Container disableGutters>
-      <H1 align="center" className={classes.title}>COVID Exposure Tracker Tool</H1>
+      <Grid container spacing={2}>
+        <Grid item xs={10}>
+          <H1 className={classes.title}>COVID Exposure Tracker Tool</H1>
+        </Grid>
+        <Grid item xs={2}>
+          <Box display="flex" justifyContent="flex-end">
+            <NotificationTray />
+          </Box>
+        </Grid>
+      </Grid>
       <DashboardButtons />
     </Container>
   );
---
+++ client/src/components/dashboard/NotificationTray.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const NotificationTray: React.FC = () => (<div>X</div>);
+
+export default NotificationTray;
