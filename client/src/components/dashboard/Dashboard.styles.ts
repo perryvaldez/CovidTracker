f8db3bc0fbc764b74d8769629f1d32050e141a4c
Perry Valdez <perryv@magenic.com>
2021-08-03 22:20:18 +0800

    Added preliminary dashboard buttons

--- client/src/components/dashboard/Dashboard.styles.ts
+++ client/src/components/dashboard/Dashboard.styles.ts
@@ -2,6 +2,12 @@ const styles = {
   title: {
     fontSize: '1.2rem',
     fontWeight: 600,
+    marginBottom: '1rem',
+  },
+
+  button: {
+      width: 100,
+      height: 90,
   },
 };
 
--- client/src/components/dashboard/Dashboard.tsx
+++ client/src/components/dashboard/Dashboard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
+import { Box, Button, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import H1 from '../markup/H1';
 import styles from './Dashboard.styles';
@@ -9,6 +10,23 @@ export const Dashboard: React.FC = () => {
   return (
     <Container disableGutters>
       <H1 align="center" className={classes.title}>COVID Exposure Tracker Tool</H1>
+      <Grid container spacing={2}>
+          <Grid item xs={4}>
+              <Box display="flex" justifyContent="center">
+                  <Button variant="contained" className={classes.button}>Add Social Interaction</Button>
+              </Box>
+          </Grid>
+          <Grid item xs={4}>
+              <Box display="flex" justifyContent="center">
+                  <Button variant="contained" className={classes.button}>Add Place Exposure</Button>
+              </Box>
+          </Grid>
+          <Grid item xs={4}>
+              <Box display="flex" justifyContent="center">
+                  <Button variant="contained" className={classes.button}>Reset Data</Button>
+              </Box>
+          </Grid>
+      </Grid>
     </Container>
   );
 };
