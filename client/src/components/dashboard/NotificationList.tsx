5703b8d677f39aa6543b30ef632d3a2c4aa25db6
Perry Valdez <perryv@magenic.com>
2021-08-03 23:40:35 +0800

    Created stub notification list

---
+++ client/src/components/dashboard/NotificationList.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import P from '../markup/P';
+
+export const NotificationList = () => (<div><P align="center">Notifications here</P></div>);
+
+export default NotificationList;
