082457b79feb0b2b1f38bdfafefe3df4d0da6aaf
Perry Valdez <perryv@magenic.com>
2021-08-05 22:55:36 +0800

    Added date picker

--- client/src/components/social_interaction/SocialInteractionDialog.tsx
+++ client/src/components/social_interaction/SocialInteractionDialog.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Grid, TextField } from '@material-ui/core';
+import { KeyboardDatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import AppDialog from '../shared/AppDialog';
+import DateFnsUtils from '@date-io/date-fns';
 
 type SocialInteractionDialogProps = {
   open: boolean,
@@ -9,6 +12,12 @@ type SocialInteractionDialogProps = {
 
 export const SocialInteractionDialog: React.FC<SocialInteractionDialogProps> = 
 ({ open, onSave, onClose }) => {
+    const [selectedDate, setSelectedDate] = useState(new Date());
+
+    const handleDateChange = (date: any) => {
+        setSelectedDate(date);
+    };
+
     return (
       <AppDialog 
         open={open} 
@@ -17,9 +26,50 @@ export const SocialInteractionDialog: React.FC<SocialInteractionDialogProps> =
         title="Add Social Interaction"
         id="id-dialog-social"
       >
-        <p>Social Interaction TODO...</p>
+        <form>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                <TextField required id="si-name" label="Name" />
+                </Grid>
+                <Grid item xs={12}>
+                    <KeyboardDatePicker
+                        variant="inline"
+                        format="MM/dd/yyyy"
+                        margin="normal"
+                        id="si-date"
+                        label="Date"
+                        value={selectedDate}
+                        onChange={handleDateChange}
+                        KeyboardButtonProps={{
+                        'aria-label': 'change date',
+                        }}
+                        autoOk
+                    />                
+                </Grid>
+            </Grid>
+          </MuiPickersUtilsProvider>
+        </form>
       </AppDialog>
     );
 };
 
 export default SocialInteractionDialog;
+
+/*
+                <!-- 
+                <KeyboardDatePicker
+                    disableToolbar
+                    variant="inline"
+                    format="MM/dd/yyyy"
+                    margin="normal"
+                    id="si-date"
+                    label="Date"
+                    value={selectedDate}
+                    onChange={handleDateChange}
+                    KeyboardButtonProps={{
+                      'aria-label': 'change date',
+                    }}
+                />                
+                --> 
+ */
