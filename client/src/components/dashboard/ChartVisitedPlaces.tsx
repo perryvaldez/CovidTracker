bc7a6e412a2d8b3e119369a79b3ac1227c3940cf
Perry Valdez <perryv@magenic.com>
2021-08-09 22:21:00 +0800

    Fixed Y axis layout

--- client/src/components/dashboard/ChartVisitedPlaces.tsx
+++ client/src/components/dashboard/ChartVisitedPlaces.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Box, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
-import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from 'recharts';
+import { Bar, BarChart, CartesianGrid, Label, XAxis, YAxis } from 'recharts';
 import H2 from '../markup/H2';
 import styles from './ChartVisitedPlaces.styles';
 import { ChartProps } from '../../lib/charting';
@@ -25,7 +25,9 @@ export const ChartVisitedPlaces: React.FC<ChartProps> = ({ data }) => {
       <BarChart width={300} height={200} data={data} margin={{ left: 0, right: 0 }}>
         <CartesianGrid strokeDasharray="4 2" />
         <XAxis dataKey="date" padding={{ left: 8, right: 8 }} />
-        <YAxis dataKey="count" label={{ value: 'No. of Visited Places', angle: -90 }} />
+        <YAxis dataKey="count" allowDecimals={false}>
+          <Label value="No. of Visited Places" angle={-90} dx={-20} />
+        </YAxis>
         <Bar dataKey="count" fill="#2b51db" />
       </BarChart>     
     </Typography>
