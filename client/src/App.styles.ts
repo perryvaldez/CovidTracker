8d390ce523bd40bd5259383c16fa5ea6855f40e2
Perry Valdez <perryv@magenic.com>
2021-08-10 13:54:29 +0800

    Improved UI

---
+++ client/src/App.styles.ts
@@ -0,0 +1,10 @@
+const styles = {
+  root: {
+    minHeight: '100vh',
+    backgroundColor: '#fff',
+    paddingLeft: 8,
+    paddingRight: 8,
+  },
+};
+
+export default styles;
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { makeStyles } from '@material-ui/styles';
 import './App.css';
 import Dashboard from './components/dashboard/Dashboard';
 import NotFound from './components/misc/NotFound';
+import styles from './App.styles';
+import { Container } from '@material-ui/core';
 
 const App = () => {
+  const classes = makeStyles(styles)();
+
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
+    <Container disableGutters className={classes.root} maxWidth="md">
+      <Router>
+        <Switch>
+          <Route exact path="/">
           <Dashboard />
-        </Route>
-        <Route path="*">
+          </Route>
+          <Route path="*">
           <NotFound />
-        </Route>
-      </Switch>
-    </Router>
+          </Route>
+        </Switch>
+      </Router>
+    </Container>
   );
 }
 
