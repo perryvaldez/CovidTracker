301403b019a927850313dff85425cf7e5c05e11f
Perry Valdez <perryv@magenic.com>
2021-08-10 17:21:48 +0800

    Designed basic notifications

---
+++ client/src/components/dashboard/NotificationList.styles.ts
@@ -0,0 +1,12 @@
+const styles = {
+  container: {
+    width: 300,
+    height: 340,
+  },
+  icon: {
+    width: 48,
+    height: 48,
+  },
+};
+
+export default styles;
--- client/src/components/dashboard/NotificationList.tsx
+++ client/src/components/dashboard/NotificationList.tsx
@@ -1,6 +1,37 @@
 import React from 'react';
-import P from '../markup/P';
+import { List, ListItem, ListItemText, makeStyles } from '@material-ui/core';
+import ErrorIcon from '@material-ui/icons/Error';
+import InfoIcon from '@material-ui/icons/Info';
+import { ListItemIcon } from '@material-ui/core';
+import styles from './NotificationList.styles';
 
-export const NotificationList = () => (<div><P align="center">Notifications here</P></div>);
+export const NotificationList = () => {
+  const classes = makeStyles(styles)();
+
+  return (
+    <div className={classes.container}>
+      <List>
+        <ListItem>
+          <ListItemIcon>
+            <ErrorIcon className={classes.icon} />
+          </ListItemIcon>
+          <ListItemText>
+            You did not practice social distancing for the last 14 days.
+            Stay at home and maintain 1-2 meters away from other people.
+          </ListItemText>
+        </ListItem>
+        <ListItem>
+          <ListItemIcon>
+            <InfoIcon className={classes.icon} />
+          </ListItemIcon>
+          <ListItemText>
+            You have been exposed to a crowded place for the last 14 days.
+            Try to avoid crowded places to minimize your exposure risk.
+          </ListItemText>
+        </ListItem>
+      </List>
+    </div>
+  );
+};
 
 export default NotificationList;
