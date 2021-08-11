ddd7f0a15fa71b98050c82a65657f61420a713bb
Perry Valdez <perryv@magenic.com>
2021-08-11 13:51:11 +0800

    Fully implemented notifications

--- client/src/components/dashboard/NotificationList.tsx
+++ client/src/components/dashboard/NotificationList.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { List, makeStyles, useMediaQuery } from '@material-ui/core';
-import styles from './NotificationList.styles';
+import { useCustomSelector } from '../../lib/hooks';
 import NotificationListItem from './NotificationListItem';
+import styles from './NotificationList.styles';
 
 export const NotificationList = () => {
   const classes = makeStyles(styles)();
@@ -14,17 +15,43 @@ export const NotificationList = () => {
     height = 180;
   }
 
+  const notificationState = useCustomSelector(state => state.notification);
+
+  let crowdedNotifItem = (
+    <NotificationListItem type="info">
+      Thank you for helping to stop spread the virus by staying home.
+    </NotificationListItem>
+  );
+
+  if(notificationState.payload.countCrowded > 0) {
+    crowdedNotifItem = (
+      <NotificationListItem type="error">
+        You have been exposed to a crowded place for the last 14 days.
+        Try to avoid crowded places to minimize your exposure risk.
+      </NotificationListItem>
+    );
+  }
+
+  let notDistancedNotifItem = (
+    <NotificationListItem type="info">
+      You are maintaining proper social distancing. Keep it up!
+    </NotificationListItem>
+  );
+
+  if(notificationState.payload.countNoSocialDistance > 0) {
+    notDistancedNotifItem = (
+      <NotificationListItem type="error">
+        You did not practice social distancing for the last 14 days.
+        Stay at home and maintain 1-2 meters away from other people.
+      </NotificationListItem>
+    );
+  }
+
   return (
     <div className={classes.container} style={{ width, height }}>
       <List>
-        <NotificationListItem type="error">
-            You did not practice social distancing for the last 14 days.
-            Stay at home and maintain 1-2 meters away from other people.
-        </NotificationListItem>
-        <NotificationListItem type="info">
-            You have been exposed to a crowded place for the last 14 days.
-            Try to avoid crowded places to minimize your exposure risk.
-        </NotificationListItem>
+        {crowdedNotifItem}
+        {notDistancedNotifItem}
       </List>
     </div>
   );
--- client/src/components/dashboard/NotificationTray.tsx
+++ client/src/components/dashboard/NotificationTray.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { Box, IconButton } from '@material-ui/core';
-import { NotificationTrayProps } from '../../lib/notification';
 import NotificationsIcon from '@material-ui/icons/Notifications';
+import { useCustomSelector } from '../../lib/hooks';
+import { NotificationTrayProps } from '../../lib/notification';
 import Counter from './Counter';
 import styles from './NotificationTray.styles';
 
 export const NotificationTray: React.FC<NotificationTrayProps> = ({ onClickedNotification }) => {
   const classes = makeStyles(styles)();
+
+  const notificationState = useCustomSelector(state => state.notification);
+  let notificationCount = 0;
+
+  if(notificationState.payload.countCrowded > 0) {
+    ++notificationCount;
+  }
+
+  if(notificationState.payload.countNoSocialDistance > 0) {
+    ++notificationCount;
+  }
+
   return (
     <Box display="flex" alignItems="center">
       <div>
@@ -22,7 +35,7 @@ export const NotificationTray: React.FC<NotificationTrayProps> = ({ onClickedNot
           <NotificationsIcon className={classes.notificationIcon} />
         </IconButton>
       </div>
-      <Counter count={0} />
+      <Counter count={notificationCount} />
     </Box>
   );
 };
