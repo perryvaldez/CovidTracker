27d2b5bc1bf1f3b31ac41b188ceb572bfdd02713
Perry Valdez <perryv@magenic.com>
2021-08-19 15:28:29 +0800

    Improved loader component

---
+++ client/src/components/shared/Loader.styles.ts
@@ -0,0 +1,13 @@
+const styles = {
+  wrapper: {
+    position: 'relative' as const,
+    opacity: 0.7,
+  },
+  progress: {
+    position: 'absolute' as const, 
+    top: 'calc(50% - 20px)', 
+    left: 'calc(50% - 20px)',
+  },
+};
+
+export default styles;
--- client/src/components/shared/Loader.tsx
+++ client/src/components/shared/Loader.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
-import P from '../markup/P';
+import classnames from 'classnames';
+import { CircularProgress, makeStyles } from '@material-ui/core';
+import styles from './Loader.styles';
 
 type LoaderProps = {
     isLoading: boolean,
+    withWrapper?: boolean,
+    wrapperClassName?: string,
 };
 
-export const Loader: React.FC<LoaderProps> = ({ children, isLoading }) => {
+export const Loader: React.FC<LoaderProps> = ({ children, isLoading, withWrapper, wrapperClassName }) => {
+    const classes = makeStyles(styles)();
+
     if (isLoading) {
+      if (withWrapper) {
         return (
-          <div>
-            <P>Loading...</P>
+          <div className={classnames(classes.wrapper, wrapperClassName)}>
+            <CircularProgress color="primary" size={40} className={classes.progress} />
+            {children}
           </div>
         );
+      }
+
+        return (
+          <div className={classnames(classes.wrapper, wrapperClassName)}>
+            <CircularProgress color="primary" size={40} className={classes.progress} />
+            {children}
+          </div>
+        );
+    }
+
+    if (withWrapper) {
+      return (<div>{children}</div>);
     }
 
     return (<>{children}</>);
